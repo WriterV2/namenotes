92ed7fcfadd637aae8a320d4130a133e6d506c7a
WriterV2 <primary-v2@protonmail.com>
2022-04-22 11:47:44 +0200

    added help text for args

--- src/cli.rs
+++ src/cli.rs
@@ -13,23 +13,23 @@ pub struct Cli {
 #[derive(Debug, Args)]
 pub struct WriteArgs {
     // required argument: "-n", "--name"
-    #[clap(short, long)]
+    #[clap(short, long, help = "Name to note down")]
     pub name: String,
 
     // optional argument: "-l", "--language"
-    #[clap(short, long)]
+    #[clap(short, long, help = "Language of name")]
     pub language: Option<String>,
 
     // optional argument: "-m", "--meaning"
-    #[clap(short, long)]
+    #[clap(short, long, help = "Meaning of name")]
     pub meaning: Option<String>,
 
     // optional argument: "-g", "--gender"
-    #[clap(short, long, arg_enum)]
+    #[clap(short, long, arg_enum, help = "Gender of name")]
     pub gender: Option<Gender>,
 
     // boolean argument: "-f", "--fictional"
-    #[clap(short, long)]
+    #[clap(short, long, help = "If name is fictional")]
     pub fictional: bool,
 }
 
@@ -37,35 +37,40 @@ pub struct WriteArgs {
 #[derive(Debug, Args)]
 pub struct ReadArgs {
     // optional argument: "-n", "--name"
-    #[clap(short, long)]
+    #[clap(short, long, help = "Filter by name")]
     pub name: Option<String>,
 
     // optional argument: "-l", "--language"
-    #[clap(short, long)]
+    #[clap(short, long, help = "Filter by language of name")]
     pub language: Option<String>,
 
     // optional argument: "-m", "--meaning"
-    #[clap(short, long)]
+    #[clap(
+        short,
+        long,
+        help = "Filter by sequence of characters in meaning of name"
+    )]
     pub meaning: Option<String>,
 
     // optional argument: "-g", "--gender"
-    #[clap(short, long, arg_enum)]
+    #[clap(short, long, arg_enum, help = "Filter by gender")]
     pub gender: Option<Gender>,
 
     // optional argument: "--length"
-    #[clap(long)]
+    #[clap(long, help = "Filter by length of name")]
     pub length: Option<u8>,
 
     // optional argument: "-c", "--contains"
-    #[clap(short, long)]
+    #[rustfmt::skip]
+    #[clap(short, long, help = "Filter by sequence of characters name has to contain")]
     pub contains: Option<String>,
 
     // optional argument: "--contains-letter"
-    #[clap(long)]
+    #[clap(long, help = "Filter by character name has to contain")]
     pub contains_letter: Option<char>,
 
     // boolean argument: "-f", "--fictional"
-    #[clap(short, long)]
+    #[clap(short, long, help = "Filter by whether name is fictional")]
     pub fictional: bool,
 }
 
